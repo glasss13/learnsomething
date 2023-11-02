050b10ad61f61fd729d7ffbe478e79bad94406d3
Max Glass <glassmax13@gmail.com>
2023-11-01 20:15:33 -0500

    Finish step 1

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,16 @@
 #include "pricingutil.h"
+
+PricingUtil::PricingUtil() : val(0) {}
+
+float PricingUtil::getVal() {
+    return val;
+}
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {
+    // Theoretical Value = (Previous Price * (0.9 + Interest Rate)) * Oleo Constant.
+    float value = prevPrice * (0.9 + interest) * oleoConstant;
+    val = value;
+    return value;
+}
+
+
--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,52 @@
 #include <gtest/gtest.h>
+#include <pricingutil.h>
 
 TEST(sampleTest, sample) {
     EXPECT_EQ(4, 4);
+}
+
+TEST(PricingUtilTest, NormalInput) {
+    PricingUtil util = PricingUtil();
+    
+    ASSERT_NEAR(util.calcVal(100.0, 0.05, 1.0), 95.0, 1e-4);
+    ASSERT_NEAR(util.getVal(), 95.0, 1e-4);
+}
+
+TEST(PricingUtilTest, ZeroInterest) {
+    PricingUtil util = PricingUtil();
+
+    ASSERT_NEAR(util.calcVal(100, 0.00, 1.0), 90.0, 1e-4);
+    ASSERT_NEAR(util.getVal(),90.0, 1e-4 );
+}
+
+
+TEST(PricingUtilTest, NegativePrevPrice) {
+    PricingUtil util = PricingUtil();
+
+    ASSERT_NEAR(util.calcVal(-100.0, 0.05, 1.0), -95.0, 1e-4);
+    ASSERT_NEAR(util.getVal(), -95.0, 1e-4);
+}
+
+
+TEST(PricingUtilTest, ZeroOleoConstant) {
+    PricingUtil util = PricingUtil();
+
+    ASSERT_NEAR(util.calcVal(100.0, 0.05, 0.00), 0.0, 1e-4);
+    ASSERT_NEAR(util.getVal(), 0.0, 1e-4);
+}
+
+
+TEST(PricingUtilTest, NegativeOleoConstant) {
+    PricingUtil util = PricingUtil();
+
+    ASSERT_NEAR(util.calcVal(100.0, 0.05, -1.0), -95.0, 1e-4);
+    ASSERT_NEAR(util.getVal(), -95, 1e-4);
+}
+
+// Test case with all zeros.
+TEST(PricingUtilTest, AllZeros) {
+    PricingUtil util = PricingUtil();
+
+    ASSERT_NEAR(util.calcVal(0.0, 0.00, 0.0), 0.0, 1e-4);
+    ASSERT_NEAR(util.getVal(), 0.0, 1e-4);
 }
